a34356a562fa3266c9adc7594438a65ff71e13f0
Jitesh4565 <jitujadhav28@gmail.com>
2024-05-04 17:49:05 +0530

    cra & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ /** @type {import('tailwindcss').Config} */
+
+ content:[ "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
